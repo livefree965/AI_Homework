8877fcc2f459eb0f0592d14d812e30fb5e770611
谢江钊 <10751583@qq.com>
2018-11-12 09:51:10 +0800

    week4 first verison

--- week_4/A_star.py
+++ week_4/A_star.py
@@ -1,12 +1,68 @@
+import copy
+
+
+def cal_estimate_cost(grid):
+    res = 0
+    for i in range(4):
+        for j in range(4):
+            if grid[i][j] != 0:
+                res += abs((grid[i][j] - 1) % 4 - j) + abs(int((grid[i][j] - 1) / 4) - i)
+    return res
+
+
 class Node:
-    def __init__(self, x, y, path_cost, parent=None):
+    def __init__(self, x, y, grid: list, path_cost, parent=None):
         self.x = x
         self.y = y
+        self.grid = grid
+        self.grid_hash = hash(str(grid))
         self.path_cost = path_cost
+        self.estimate_cost = cal_estimate_cost(self.grid)
         self.parent = parent
 
 
-puzzle_data = [[11, 3, 1, 7], [4, 6, 8, 2], [15, 9, 10, 13], [14, 12, 5, 0]]
+def is_hash_in_list(obj_hash, src_list):
+    for grid in src_list:
+        if obj_hash == grid.grid_hash:
+            return grid
+    return False
 
-open_list = []
-close_list = []
+
+sample_data = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 0, 15]]
+puzzle_data = [[11, 3, 1, 7], [4, 6, 8, 2], [15, 9, 10, 13], [14, 12, 5, 0]]
+goal_data = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]
+goal_hash = hash(str(goal_data))
+if __name__ == "__main__":
+    begin_node = Node(3, 2, puzzle_data, 0)
+    open_list = [begin_node]
+    close_list = []
+    while len(open_list) > 0:
+        open_list.sort(key=lambda x: x.path_cost + x.estimate_cost)
+        now_node = open_list.pop(0)
+        print('now path cost: ', now_node.path_cost)
+        if now_node.grid_hash == goal_hash:
+            break
+        close_list.append(now_node)
+        actions = [[0, 1], [0, -1], [-1, 0], [1, 0]]
+        for action in actions:
+            new_pos = [action[0] + now_node.x, action[1] + now_node.y]
+            if 0 < new_pos[0] < 4 and 0 < new_pos[1] < 4:
+                new_grid = copy.deepcopy(now_node.grid)
+                new_grid[new_pos[0]][new_pos[1]] = 0
+                new_grid[now_node.x][now_node.y] = now_node.grid[new_pos[0]][new_pos[1]]
+                new_grid_hash = hash(str(new_grid))
+                if is_hash_in_list(new_grid_hash, close_list):
+                    continue
+                if is_hash_in_list(new_grid_hash, open_list):
+                    old_node = is_hash_in_list(new_grid_hash, open_list)
+                    old_g = old_node.estimate_cost
+                    new_g = cal_estimate_cost(new_grid)
+                    if new_g < old_g:
+                        old_node.estimate_cost = new_g
+                        old_node.parent = now_node
+                        pass
+                else:
+                    new_node = Node(new_pos[0], new_pos[1], new_grid, now_node.path_cost + 1, parent=now_node)
+                    open_list.append(new_node)
+            else:
+                continue
