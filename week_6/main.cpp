a9d90cdda105af81f9204fa9d2d144624c5ece9e
谢江钊 <10751583@qq.com>
2018-12-04 15:32:09 +0800

    update comment

--- week_6/main.cpp
+++ week_6/main.cpp
@@ -100,16 +100,20 @@ bool constraint_check() {
 }
 
 void mark_remove(int pos, int level) {
+    //下棋后，更新棋盘上不可取的点，等价于更新其他棋子的值域
+    //更新行上的值
     int i = pos % GRID_SIZE;
     for (; i < GRID_SIZE * GRID_SIZE; i += GRID_SIZE) {
         if (grid[i] == 0)
             grid[i] = level;
     }
+    //更新列上的值
     i = (pos / GRID_SIZE) * GRID_SIZE;
     for (; i < (pos / GRID_SIZE) * GRID_SIZE + GRID_SIZE; i++) {
         if (grid[i] == 0)
             grid[i] = level;
     }
+    //更新 / 方向上的值
     i = pos - min(pos % GRID_SIZE, pos / GRID_SIZE) * (GRID_SIZE + 1);
     for (; i < GRID_SIZE * GRID_SIZE; i += GRID_SIZE + 1) {
         if (grid[i] == 0)
@@ -117,6 +121,7 @@ void mark_remove(int pos, int level) {
         if (i % GRID_SIZE == GRID_SIZE - 1)
             break;
     }
+    //更新 \ 方向上的值
     i = pos - min((GRID_SIZE - (pos % GRID_SIZE) - 1), (pos / GRID_SIZE)) * (GRID_SIZE - 1);
     for (; i < GRID_SIZE * GRID_SIZE; i += GRID_SIZE - 1) {
         if (grid[i] == 0)
@@ -127,6 +132,7 @@ void mark_remove(int pos, int level) {
 }
 
 void unmark_remove(int pos, int level) {
+    //反向操作，回溯后撤去棋子更新值域
     for (int i = 0; i < GRID_SIZE * GRID_SIZE; ++i) {
         if (grid[i] == level)
             grid[i] = 0;
@@ -136,42 +142,38 @@ void unmark_remove(int pos, int level) {
 bool forward_check(int level) {
     bool res = false;
     for (int i = level * GRID_SIZE; i < GRID_SIZE * GRID_SIZE; i += GRID_SIZE) {
-        res = false;
+        res = false;//首先认为后续的某一行棋子发生了DWO
         for (int j = 0; j < GRID_SIZE; ++j) {
-            if (grid[i + j] == 0) {
-                res = true;
-                break;
+            if (grid[i + j] == 0) { //棋子有可以取值的点，值域不为空
+                res = true; //更新res
+                break;  //退出这一行棋子的循环
             }
         }
-        if (!res)
-            return false;
+        if (!res)   //如果这一行的棋子找不到下棋点，发生了DWO
+            return false;   //返回失败
     }
-    return true;
+    return true;    //所有后续行的棋子值域非空，返回真
 }
 
 
 void fc_method(int level) {
-//    printf("%d\n", level);
     if (level == OBJ_LEVEL) {
         int *res = new int[GRID_SIZE * GRID_SIZE];
         memcpy(res, grid, sizeof(int) * GRID_SIZE * GRID_SIZE);
         ans.push_back(res);
-//        exit(0);
         return;
-    }
+    }   //成功找到解
     int pos = level * GRID_SIZE;
-    for (; pos < (level + 1) * GRID_SIZE; ++pos) {
+    for (; pos < (level + 1) * GRID_SIZE; ++pos) {  //搜索当前行
         if (grid[pos] == 0) {
-            grid[pos] = 1;
-            mark_remove(pos, -level - 1);
-            if (forward_check(level + 1)) {
-//                printf("--------------------------------------------------\n");
-//                show_grid();
-                fc_method(level + 1);
-                unmark_remove(pos, -level - 1);
-                grid[pos] = 0;
+            grid[pos] = 1;  //放置棋子
+            mark_remove(pos, -level - 1);   //更新值域
+            if (forward_check(level + 1)) { //如果不发生DWO
+                fc_method(level + 1);   //搜下一层棋子
+                unmark_remove(pos, -level - 1); //回溯，撤销值域修改
+                grid[pos] = 0;  //删去棋子
             } else {
-                unmark_remove(pos, -level - 1);
+                unmark_remove(pos, -level - 1); //发生DWO，提前回溯
                 grid[pos] = 0;
             }
         }
@@ -180,23 +182,21 @@ void fc_method(int level) {
 
 
 void backtrack(int level) {
-    if (level == OBJ_LEVEL) {
-        //            printf("--------------------------------------------------\n");
-        //            show_grid();
+    if (level == OBJ_LEVEL) {   //搜到了目标层次，所有皇后已经在棋盘上
         int *res = new int[GRID_SIZE * GRID_SIZE];
-        memcpy(res, grid, sizeof(int) * GRID_SIZE * GRID_SIZE);
-        ans.push_back(res);
+        memcpy(res, grid, sizeof(int) * GRID_SIZE * GRID_SIZE);//复制棋盘状态到新的数组上
+        ans.push_back(res);//将数组放入解的集合
         return;
     }
-    int pos = level * GRID_SIZE;
+    int pos = level * GRID_SIZE;    // 皇后是一行一行放置的，根据当前搜索深度确定要尝试放置的行数。
     for (; pos < (level + 1) * GRID_SIZE; ++pos) {
-        if (grid[pos] == 0) {
-            grid[pos] = 1;
-            if (constraint_check()) {
-                backtrack(level + 1);
-                grid[pos] = 0;
+        if (grid[pos] == 0) {   //改为之上没有棋子才可以进行放置
+            grid[pos] = 1;  //放下棋子
+            if (constraint_check()) {   //检查是否满足约束
+                backtrack(level + 1);   //满足约束则进行下一层的放置
+                grid[pos] = 0;      //回溯，撤销棋子的放置
             } else
-                grid[pos] = 0;
+                grid[pos] = 0;  //不满足约束，取消放置
         }
     }
 }
