1cccf09b4dbd3c4702a6e53441d3a236c8cce6ab
谢江钊 <10751583@qq.com>
2018-12-18 17:36:36 +0800

    Update task_1.py

--- week_8/task_1.py
+++ week_8/task_1.py
@@ -1,37 +1,33 @@
-import copy
-
-
 class VariableElimination:
     @staticmethod
     def inference(factor_list, query_variables,
                   ordered_list_of_hidden_variables, evidence_list):
         # 对evidence进行消除
         for ev in evidence_list:  # 对每一个evidence进行处理
-            VariableElimination.print_factors(factor_list)
-            new_factor_list = []  # 存放不影响的节点
-            tmp_factor_list = []  # 存放影响的节点
+            include_list = []  # 存放不影响的节点
+            exclude_list = []  # 存放影响的节点
             for factor in factor_list:
                 if ev in factor.var_list:  # 与evidence相关
-                    tmp_factor_list.append(factor)
+                    exclude_list.append(factor)
                 else:  # 与evidence无关
-                    new_factor_list.append(factor)
-            for factor in tmp_factor_list[1:]:  # 将相关变量相乘，求出新的概率
-                tmp_factor_list[0] = tmp_factor_list[0].multiply(factor)  # 相乘
-            new_factor_list.append(tmp_factor_list[0].restrict(ev, evidence_list[ev]))  # 消去相关的变量并加入回列表中
-            factor_list = new_factor_list  # 赋值，对下一个evidence进行处理
+                    include_list.append(factor)
+            for factor in exclude_list[1:]:  # 将相关变量相乘，求出新的概率
+                exclude_list[0] = exclude_list[0].multiply(factor)  # 相乘
+            include_list.append(exclude_list[0].restrict(ev, evidence_list[ev]))  # 消去相关的变量并加入回列表中
+            factor_list = include_list  # 赋值，对下一个evidence进行处理
             # Your code here
         for var in ordered_list_of_hidden_variables:
-            new_factor_list = []
-            tmp_factor_list = []
+            include_list = []
+            exclude_list = []
             for factor in factor_list:
                 if var in factor.var_list:
-                    tmp_factor_list.append(factor)
+                    exclude_list.append(factor)
                 else:
-                    new_factor_list.append(factor)
-            for factor in tmp_factor_list[1:]:
-                tmp_factor_list[0] = tmp_factor_list[0].multiply(factor)
-            new_factor_list.append(tmp_factor_list[0].sum_out(var))  # 同上，逐个消去变量
-            factor_list = new_factor_list
+                    include_list.append(factor)  # 跟上面的做类似的处理
+            for factor in exclude_list[1:]:
+                exclude_list[0] = exclude_list[0].multiply(factor)
+            include_list.append(exclude_list[0].sum_out(var))  # 同上，逐个消去变量
+            factor_list = include_list
             # Your code here
         print("RESULT: ")
         res = factor_list[0]
@@ -74,32 +70,21 @@ class Node:
         print()
 
     def multiply(self, factor):
-        # tmp_list = ['B', 'E', 'A', 'J', 'M']
-        tmp_list = Util.get_union(self.var_list, factor.var_list)
-        new_list = []
-        new_mark = []
+        tmp_list = Util.get_union(self.var_list, factor.var_list)  # 对两个节点的var_list进行union操作
         len_key = len(tmp_list)
-        for var in tmp_list:
-            if var in self.var_list and var in factor.var_list:
-                new_mark.append(0)
-            elif var in self.var_list:
-                new_mark.append(1)
-            else:
-                new_mark.append(2)
         new_list = tmp_list
         new_cpt = {}
-
-        left_index = [tmp_list.index(i) for i in self.var_list]
-        right_index = [tmp_list.index(i) for i in factor.var_list]
-        for i in range(pow(2, len_key)):
-            tmp_key = Util.to_binary(i, len_key)
-            left_key = ''
-            right_key = ''
-            for key_pos in left_index:
+        left_index = [tmp_list.index(i) for i in self.var_list]  # 本节点的每个变量在tmp_list中对应的下标
+        right_index = [tmp_list.index(i) for i in factor.var_list]  # factor节点的每个变量在tmp_list中对应的下标
+        for i in range(pow(2, len_key)):  # 对于n个变量，有2^n个可能的结果的组合
+            tmp_key = Util.to_binary(i, len_key)  # 使用函数转换为二进制字符串
+            left_key = ''  # 对应本节点的cpt的对应key值
+            right_key = ''  # 对应factor节点的cpt的对应key值
+            for key_pos in left_index:  # 根据以上计算的下标计算key
                 left_key += tmp_key[key_pos]
             for key_pos in right_index:
                 right_key += tmp_key[key_pos]
-            new_cpt[tmp_key] = self.cpt[left_key] * factor.cpt[right_key]
+            new_cpt[tmp_key] = self.cpt[left_key] * factor.cpt[right_key]  # 放入new_cpt中
         '''function that multiplies with another factor'''
         # Your code here
         new_node = Node('f' + str(new_list), new_list)
@@ -107,22 +92,17 @@ class Node:
         return new_node
 
     def sum_out(self, variable):
-        var_i = 0
-        new_var_list = []
-        for i in range(len(self.var_list)):
-            if self.var_list[i] != variable:
-                new_var_list.append(self.var_list[i])
-            else:
-                var_i = i
+        variable_index = self.var_list.index(variable)  # 需要求和的目标变量下标
+        new_var_list = [var for var in self.var_list if var != variable]  # 消去后的变量集合
         new_cpt = {}
         for key in self.cpt.keys():
-            tmp_key = key[:var_i] + key[var_i + 1:]
-            if tmp_key in new_cpt.keys():
+            tmp_key = key[:variable_index] + key[variable_index + 1:]  # 消去目标变量，这里对概率的key进行处理
+            if tmp_key in new_cpt.keys():  # 如果key值已经存在，则跳过
                 continue
-            new_cpt[tmp_key] = 0
-            for sec_key in self.cpt.keys():
-                if sec_key[:var_i] + sec_key[var_i + 1:] == tmp_key:
-                    new_cpt[tmp_key] += self.cpt[sec_key] / 2
+            new_cpt[tmp_key] = 0  # 初始化键值
+            for other_key in self.cpt.keys():  # 对于剩余的每一个键值对，如果删除了目标变量，其余变量一致，则键值相加
+                if other_key[:variable_index] + other_key[variable_index + 1:] == tmp_key:
+                    new_cpt[tmp_key] += self.cpt[other_key] / 2  # 注意要除以2，否则概率会大于1
         '''function that sums out a variable given a factor'''
         # Your code here
         new_node = Node('f' + str(new_var_list), new_var_list)
@@ -130,18 +110,13 @@ class Node:
         return new_node
 
     def restrict(self, variable, value):
-        var_i = 0
-        new_var_list = []
-        for i in range(len(self.var_list)):
-            if self.var_list[i] != variable:
-                new_var_list.append(self.var_list[i])
-            else:
-                var_i = i
+        value_index = self.var_list.index(variable)  # 需要处理的目标变量下标
+        new_var_list = [var for var in self.var_list if var != variable]  # 消去后的变量集合
         new_cpt = {}
         for key in self.cpt:
-            if key[var_i] == str(value):
-                tmp_key = key[:var_i] + key[var_i + 1:]
-                new_cpt[tmp_key] = self.cpt[key] * 2
+            if key[value_index] == str(value):  # 满足给定的value才保留这个概率
+                tmp_key = key[:value_index] + key[value_index + 1:]
+                new_cpt[tmp_key] = self.cpt[key] * 2  # 注意要乘2，否则总的概率加起来将不足1
         '''function that restricts a variable to some value
         in a given factor'''
         # Your code here
